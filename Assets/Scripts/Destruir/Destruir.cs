f37c9863a5d1c611ad7219672d810c8cc7fdb7fe
Camilo romero <yhersson_romero@hotmail.com>
2026-09-15 13:41:48 -0500

    Drop de barriles y cajas
    Se agrego el drop de monedas en los prefabs  barriles y cajas

--- Assets/Scripts/Destruir/Destruir.cs
+++ Assets/Scripts/Destruir/Destruir.cs
@@ -2,17 +2,24 @@ using UnityEngine;
 
 public class Destructible : MonoBehaviour
 {
-    
+
     /// Destruye el objeto.
     /// Más adelante aquí agregaremos:
     /// - Sonido de destrucción.
     /// - Partículas.
     /// - Animación.
-    /// - Objetos que suelte (loot).
-  
+
     public void Romper()
     {
         Debug.Log(gameObject.name + " ha sido destruido.");
+
+        // Soltamos monedas si el objeto tiene el script.
+        SoltarMonedas soltarMonedas = GetComponent<SoltarMonedas>();
+
+        if (soltarMonedas != null)
+        {
+            soltarMonedas.Soltar();
+        }
 
         Destroy(gameObject);
     }
